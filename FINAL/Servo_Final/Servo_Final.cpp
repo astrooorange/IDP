4238b18be10371bcc6cb9fdd2c3abad567a261d3
astrooorange <157716707+astrooorange@users.noreply.github.com>
2024-11-29 09:57:48 +0000

    Update Servo_Final.cpp

--- FINAL/Servo_Final/Servo_Final.cpp
+++ FINAL/Servo_Final/Servo_Final.cpp
@@ -3,8 +3,7 @@
 #include "Line_Following_Final.h"
 
 void grab_object()
-{
-  
+{  
   for(int i = 0 ;i <6 ;i++)      // Tries 5 times to sweep in, so the object IS in
   {
   myservo.write(80);      // Attempts to sweep in the object
@@ -20,6 +19,9 @@ void drop_off() // Function to drop the object whenever the robot is at the junc
 {
   // IF the robot isn't going forward/backward in a straight enough manner i think we can reduce the speed of it going forward and backward so that its slow enough to where like stuff should hit it off or smth (if rlly bad just change each wheel to spin at different amounts)
 
+  
+  // robot enters the site, initially facing the site
+  
   unsigned long startTime=millis();     // Variable to store the start time
   unsigned long runDuration = 1000 ;      // Time we want the program to run for (in milliseconds)
   while(millis() - startTime < runDuration);
@@ -27,16 +29,22 @@ void drop_off() // Function to drop the object whenever the robot is at the junc
     chassis_forward();
   }
 
+    ml -> run(RELEASE);
+    mr -> run(RELEASE);
+
+  //robot drops the object
+  
     myservo.write(-20);     // Spinning the Servos to push the waste out of the catchment     
     myservo.write(80);      // Back to initial
 
     delay(15);
 
-
-  startTime=millis();     // Variable to store the start time
-  runDuration = 1800 ;      // Time we want the program to run for (in milliseconds)
+  //robot returns to initial position with the same orientation
   while(millis() - startTime < runDuration);
   {
     chassis_backward();
   }
+
+    ml -> run(RELEASE);
+    mr -> run(RELEASE);
 }
