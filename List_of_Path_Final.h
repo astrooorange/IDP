fa8ca63d8ee15d370b9901fb6e9b8adbcf086f55
astrooorange <157716707+astrooorange@users.noreply.github.com>
2024-12-03 15:30:11 +0000

    FINAL_NEW

---
+++ List_of_Path_Final.h
@@ -0,0 +1,13 @@
+#ifndef LIST_OF_PATH_FINAL_H
+#define LIST_OF_PATH_FINAL_H
+
+// Declaration of global variables
+extern int magnetic_sensor_pin;
+extern int sl1;
+extern int sr1;
+
+// Declaration of function
+void execute_list(int list[], int listSize);
+
+
+#endif
